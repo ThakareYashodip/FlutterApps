d275130483e84f7109371f333ba8611285f150e5
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2024-09-23 23:29:54 +0530

    basic app

---
+++ ClassApps/container_row_or_column/README.md
@@ -0,0 +1,3 @@
+# container_row_or_column
+
+A new Flutter project.
---
+++ ClassApps/container_row_or_column/android/app/src/main/kotlin/com/example/container_row_or_column/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.container_row_or_column
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
