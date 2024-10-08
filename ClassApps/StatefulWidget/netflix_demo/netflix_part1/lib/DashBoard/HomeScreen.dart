6bc8745c60e91ab046d14d6de124ffb8d5ac1bf3
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2024-10-08 16:12:40 +0530

    netflix

--- ClassApps/StatefulWidget/netflix_demo/netflix_part1/lib/DashBoard/HomeScreen.dart
+++ ClassApps/StatefulWidget/netflix_demo/netflix_part1/lib/DashBoard/HomeScreen.dart
@@ -50,226 +50,243 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Container(
-                decoration: const BoxDecoration(color: Colors.black),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            print("TV Shows");
-                          },
-                          child: const SizedBox(
-                            height: 30,
-                            child: Text(
-                              "TV Shows",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500),
+          Container(
+            child: Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.black),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              print("TV Shows");
+                            },
+                            child: const SizedBox(
+                              height: 30,
+                              child: Text(
+                                "TV Shows",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            print("Movies");
-                          },
-                          child: const SizedBox(
-                            height: 30,
-                            child: Text(
-                              "Movies",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500),
+                          GestureDetector(
+                            onTap: () {
+                              print("Movies");
+                            },
+                            child: const SizedBox(
+                              height: 30,
+                              child: Text(
+                                "Movies",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            print("Categories");
-                          },
-                          child: const SizedBox(
-                            height: 30,
-                            child: Text(
-                              "Categories",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500),
+                          GestureDetector(
+                            onTap: () {
+                              print("Categories");
+                            },
+                            child: const SizedBox(
+                              height: 30,
+                              child: Text(
+                                "Categories",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        print(" Poster Clicked ");
-                      },
-                      child: SizedBox(
-                        height: 580,
-                        width: 380,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: MoviePosters.length,
-                          physics: const BouncingScrollPhysics(),
-                          itemBuilder: (BuildContext context, int index) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  height: 550,
-                                  width: 350,
-                                  decoration: BoxDecoration(
-                                    border:
-                                        Border.all(width: 0.3, color: Colors.grey),
-                                    borderRadius: BorderRadius.circular(15),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          print(" Poster Clicked ");
+                        },
+                        child: SizedBox(
+                          height: 580,
+                          width: 380,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: MoviePosters.length,
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (BuildContext context, int index) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: 550,
+                                    width: 350,
+                                    decoration: BoxDecoration(
+                                      border:
+                                          Border.all(width: 0.3, color: Colors.grey),
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    margin: const EdgeInsets.all(10),
+                                    clipBehavior: Clip.hardEdge,
+                                    child: Image.network(
+                                      MoviePosters[index]['imageUrl']!,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                  margin: const EdgeInsets.all(10),
-                                  clipBehavior: Clip.hardEdge,
-                                  child: Image.network(
-                                    MoviePosters[index]['imageUrl']!,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
+                                ],
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            print(" Button Pressed ");
-                          },
-                          child: const Column(
-                            children: [
-                              Icon(
-                                Icons.add,
-                                size: 30,
-                                color: Colors.white,
-                              ),
-                              Text(
-                                "MyList",
-                                style: TextStyle(fontSize: 15, color: Colors.white),
-                              ),
-                            ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const SizedBox(
+                            width: 10,
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            height: 40,
-                            width: 100,
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.all(Radius.circular(12))),
-                            child: const Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                          GestureDetector(
+                            onTap: () {
+                              print(" Button Pressed ");
+                            },
+                            child: const Column(
                               children: [
-                                SizedBox(
-                                  width: 10,
-                                ),
                                 Icon(
-                                  Icons.play_arrow,
+                                  Icons.add,
                                   size: 30,
-                                  color: Colors.black,
+                                  color: Colors.white,
                                 ),
                                 Text(
-                                  "Play",
-                                  style: TextStyle(fontSize: 18, color: Colors.black),
+                                  "MyList",
+                                  style: TextStyle(fontSize: 15, color: Colors.white),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: const Column(
-                            children: [
-                              Icon(
-                                Icons.info,
-                                size: 30,
-                                color: Colors.white,
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 40,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(Radius.circular(12))),
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(
+                                    Icons.play_arrow,
+                                    size: 30,
+                                    color: Colors.black,
+                                  ),
+                                  Text(
+                                    "Play",
+                                    style: TextStyle(fontSize: 18, color: Colors.black),
+                                  ),
+                                ],
                               ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Column(
+                              children: [
+                                Icon(
+                                  Icons.info,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  "Info",
+                                  style: TextStyle(fontSize: 15, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: MoviePosters.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
                               Text(
-                                "Info",
-                                style: TextStyle(fontSize: 15, color: Colors.white),
+                                "  ${MoviePosters[index]['name']}",
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 180,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: MoviePosters[index]['Genre'].length,
+                                  itemBuilder: (BuildContext context, int innerIndex) {
+                                    return Column(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 5, vertical: 10),
+                                          child: Image.network(
+                                            MoviePosters[index]['Genre'][innerIndex],
+                                            fit: BoxFit.fill,
+                                            height: 160,
+                                            width: 120,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                ),
                               ),
                             ],
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: MoviePosters.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "  ${MoviePosters[index]['name']}",
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 180,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: MoviePosters[index]['Genre'].length,
-                                itemBuilder: (BuildContext context, int innerIndex) {
-                                  return Column(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 5, vertical: 10),
-                                        child: Image.network(
-                                          MoviePosters[index]['Genre'][innerIndex],
-                                          fit: BoxFit.fill,
-                                          height: 160,
-                                          width: 120,
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                    const Icon(Icons.navigate_before_rounded,size: 30,color: Colors.white,)
-                  ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
+          Container(
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: IconButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    }, 
+                    icon: const Icon(Icons.navigate_before_rounded,size: 40,color: Colors.white,),),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
