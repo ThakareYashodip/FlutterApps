7ea2535fcfed505681162dea96c790e5ab10cd7a
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2024-10-13 14:59:30 +0530

    .

--- Assignment/daily_flash_2/lib/page5.dart
+++ Assignment/daily_flash_2/lib/page5.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-String? str = "Click Me!";
-bool flag1 = true;
 
-Widget Scaffold5(){
-  return Scaffold(
+class Scaffold5 extends StatefulWidget {
+  const Scaffold5({super.key});
+
+  @override
+  State<Scaffold5> createState() => _Scaffold5State();
+}
+
+class _Scaffold5State extends State<Scaffold5> {
+
+  String str = "Click Me";
+  bool flag1 = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     backgroundColor: Colors.black54,
     body: Center(
       child: GestureDetector(
@@ -16,22 +27,24 @@ Widget Scaffold5(){
             flag1 = true;
             str = "Click Me";
           }
+          setState(() {});
         },
         child: Container(
           width: 200,
           height: 200,
           decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 226, 151, 38),
+            color: (flag1)?  Colors.yellow: const Color.fromARGB(255, 226, 151, 38),
             border: Border.all(color: const Color.fromARGB(255, 215, 37, 64),width: 5),
             borderRadius: const BorderRadius.only(topLeft: Radius.circular(20),bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20),topRight: Radius.circular(20))
           ),
           child: Center(
-            child: Text(str!,style: const TextStyle(
-              fontSize: 30,fontWeight: FontWeight.bold, color: Colors.white
+            child: Text(str,style: const TextStyle(
+              fontSize: 30,fontWeight: FontWeight.bold, color: Colors.black
             ),),
           ),
         ),
       ),
     ),
   );
+  }
 }
